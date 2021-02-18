41dc196811d64168350b8465155c196a5ad6c22f
Javier Arturo Hernández Sosa <punkrock.arturito@gmail.com>
2021-02-18 02:07:28 -0600

    Add files via upload

---
+++ veneno.py
@@ -0,0 +1,12 @@
+import pickle
+import pandas as pd
+
+class Veneno:
+    def __init__(self, num):
+        with open('distance.sav', 'rb') as f:
+            self.distance = pickle.load(f)
+        self.num = num
+    
+    def csv(self):
+        s = pd.Series(self.distance.loc[self.distance.index!=9999,9999].sort_values().head(self.num).index)
+        s.to_csv("urgente_orden_de_cierre_test.csv", index=False)
